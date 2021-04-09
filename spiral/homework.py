110bc7c2d0547a92514cb6cb68ac8a72c7988217
KalahBranch <branchk@vcu.edu>
2021-04-08 23:36:05 -0400

    Push 1

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,12 @@
 def spiralize(number):
-    return_value = 1
-
+    
+    if number == 1:
+        return_value = 1
+    else: 
+        return_value = ( 2 * (number * number * 2 )  - (2 * number - 2) * 3 + spiralize(number - 2))
     return return_value
+
+if __name__ == "__main__":
+    print(spiralize(5)) 
+    print(spiralize(21))
+    print(spiralize(31))
