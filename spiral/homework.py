33ae4c543b79e4c204230a68b49349d3ada093ab
KalahBranch <branchk@vcu.edu>
2021-04-08 23:44:34 -0400

    Black Changes

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,12 +1,15 @@
 def spiralize(number):
-    
+
     if number == 1:
         return_value = 1
-    else: 
-        return_value = ( 2 * (number * number * 2 )  - (2 * number - 2) * 3 + spiralize(number - 2))
+    else:
+        return_value = (
+            2 * (number * number * 2) - (2 * number - 2) * 3 + spiralize(number - 2)
+        )
     return return_value
 
+
 if __name__ == "__main__":
-    print(spiralize(5)) 
+    print(spiralize(5))
     print(spiralize(21))
     print(spiralize(31))
